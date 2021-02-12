eaf2f240af12e402c09239d70c33fea43e762fee
dbray7 <devenbray@gmail.com>
2021-02-12 15:56:21 -0500

    another change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ class Home extends Component {
     return (
       <div>
         <Navbar />
-        <Jumbotron title="Welcome" />
+        <Jumbotron title="Fancy seeing you here!" />
         <div className="container">
           <div className="col-8">
             <h2>Hello!</h2>
